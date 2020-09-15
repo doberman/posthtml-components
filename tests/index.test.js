67aff0736cfa08ee557f0d6eb64202c19c545c7e
Daniel Hägglund <daniel.hagglund@doberman.se>
2020-09-15 14:57:54 +0200

    Add test with inner content

--- tests/index.test.js
+++ tests/index.test.js
@@ -13,3 +13,14 @@ it("should include html", async () => {
     .then((result) => clean(result.html));
   expect(html).toEqual(expected);
 });
+
+it('should include content', async () => {
+
+  const actual = `<div><my-wrap>Test content</my-wrap></div>`;
+  const expected = `<div><div class="wrap">Test content</div></div>`;
+  const html = await posthtml()
+    .use(plugin(options))
+    .process(actual)
+    .then((result) => clean(result.html));
+  expect(html).toEqual(expected);
+})
